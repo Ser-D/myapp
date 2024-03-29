c744ae51312d7a38ba7fd5ec57155f08b1656d28
Ser-D <ipc.avanti@gmail.com>
2024-03-29 15:51:40 +0200

    First Pagination option

--- goods/views.py
+++ goods/views.py
@@ -1,16 +1,23 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator
+from django.shortcuts import render, get_object_or_404
 
 from goods.models import Products
 
 
-def catalog(request):
+def catalog(request, category_slug, page=1):
 
-    goods = Products.objects.all()
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods: Products = get_object_or_404(Products.objects.filter(category__slug=category_slug))
+
+    paginator = Paginator(goods, 3)
+    current_page = paginator.page(page)
 
     context: dict[str, str] = {
         "title": "Home - Каталог",
         "content": "Про нас",
-        "goods": goods, 
+        "goods": current_page, 
            
     }
     return render(request, "goods/catalog.html", context)
