0fe4e12e8f037eeef2be77db85ec2a1b4e9fc654
Ser-D <ipc.avanti@gmail.com>
2024-04-05 10:25:35 +0300

    templftes for user app

--- users/views.py
+++ users/views.py
@@ -4,21 +4,21 @@ def login(request):
     context = {
         'title': 'Home - Авторизація'
     }
-    return render(request, 'user/login.html', context)
+    return render(request, 'users/login.html', context)
 
 
 def registration(request):
     context = {
         'title': 'Home - Реєстрація'
     }
-    return render(request, 'user/registration.html', context)
+    return render(request, 'users/registration.html', context)
 
 
 def profile(request):
     context = {
         'title': 'Home - Кабінет'
     }
-    return render(request, 'user/profile.html', context)
+    return render(request, 'users/profile.html', context)
 
 
 def logout(request):
