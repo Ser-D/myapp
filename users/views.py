a38a521d88983c86176a32299762291506b9e2ef
Ser-D <ipc.avanti@gmail.com>
2024-04-05 18:00:16 +0300

    1

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
-from users.forms import UserLoginForm, UserRegistrationForm
+from users.forms import UserLoginForm, UserRegistrationForm, ProfileForm
 
 def login(request):
     if request.method == 'POST':
@@ -44,8 +44,16 @@ def registration(request):
 
 
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
     context = {
-        'title': 'Home - Кабінет'
+        'title': 'Home - Кабінет',
+        'form': form
     }
     return render(request, 'users/profile.html', context)
 
