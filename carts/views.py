7c682b573f5e259e4e7671c722533f26a33eb4bd
Ser-D <ipc.avanti@gmail.com>
2024-04-16 20:02:26 +0300

    Cart templates were included

---
+++ carts/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+
+def cart_add(request, producn_id):
+    ...
+
+
+def cart_change(request, producn_id):
+    ...
+
+
+def cart_remove(request, producn_id):
+    ...
