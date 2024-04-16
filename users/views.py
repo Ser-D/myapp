f1b2d90f11bfc5901c064879c59b32dcbc7ce475
Ser-D <ipc.avanti@gmail.com>
2024-04-16 22:21:08 +0300

    add to cart view, refactor included cart templates

--- users/views.py
+++ users/views.py
@@ -16,6 +16,12 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f'{username}, Ви увійшли в акаунт')
+                
+                redirect_page = request.POST.get('next', None)
+                
+                if redirect_page and redirect_page != reverse('user:logout'):
+                    return HttpResponseRedirect(request.POST.get('next'))
+                    
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
